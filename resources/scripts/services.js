89f4fb459871f0b0f5773b0f6cf8a8c648111336
Wolfgang Meier <wolfgang@exist-db.org>
2017-09-16 18:06:15 +0200

    Query APIs in sequence to avoid server overload

--- resources/scripts/services.js
+++ resources/scripts/services.js
@@ -10,114 +10,82 @@ $(document).ready(function() {
         $("span[data-ref='" + key + "']").text(label);
     }
 
-    $(".places li").each(function() {
-        var elem = $(this);
+    function updatePlace(elem, entry) {
         var key = elem.attr("data-ref");
-        $.ajax({
-            url: PLACES_API,
-            data: {
-                "id": key
-            },
-            dataType: "json",
-            success: function(entry) {
-                if (entry.type) {
-                    elem.find("a").text(entry.stdName['#text']);
-                    if (entry.location) {
-                        elem.append($('<span class="location"></span>').text(entry.location));
-                    }
-                    elem.append($('<span class="type"></span>').text(entry.type));
-
-                    var label = entry.stdName['#text'] + ' (' + entry.location + '), ' +
-                        entry.type;
-                    updateSpans(key, label);
-                }
+        if (entry.type) {
+            elem.find("a").text(entry.stdName['#text']);
+            if (entry.location) {
+                elem.append($('<span class="location"></span>').text(entry.location));
             }
-        });
+            elem.append($('<span class="type"></span>').text(entry.type));
+
+            var label = entry.stdName['#text'] + ' (' + entry.location + '), ' +
+                entry.type;
+            updateSpans(key, label);
+        }
+    }
+
+    function query(uri, param, list, callback) {
+        var head = list.shift();
+        if (head) {
+            var key = $(head).attr("data-ref");
+            var params = {};
+            params[param] = key;
+            $.ajax({
+                url: uri,
+                data: params,
+                dataType: "json",
+                success: function(data) {
+                    query(uri, param, list, callback);
+                    callback($(head), data);
+                }
+            });
+        }
+    }
+
+    query(PLACES_API, "id", $(".places li").toArray(), updatePlace);
+    query(PERSON_API, "query", $(".persons li").toArray(), function(elem, entry) {
+        if (entry.name) {
+            elem.find("a").text(entry.name);
+            if (entry.dates) {
+                elem.append($('<span class="info"></span>').text(entry.dates));
+            }
+
+            updateSpans(key, entry.name + ' (' + entry.dates + ')');
+        }
+    });
+    query(PERSON_API, "query", $(".organizations li").toArray(), function(elem, entry) {
+        if (entry.name) {
+            elem.find("a").text(entry.name);
+            if (entry.type) {
+                elem.append($('<span class="info"></span>').text(entry.type));
+            }
+
+            updateSpans(key, entry.name + ' (' + entry.type + ')');
+        }
     });
 
-    $(".persons li").each(function() {
-        var elem = $(this);
-        var key = elem.attr("data-ref");
-        $.ajax({
-            url: PERSON_API,
-            data: {
-                "query": key
-            },
-            dataType: "json",
-            success: function(entry) {
-                if (entry.name) {
-                    elem.find("a").text(entry.name);
-                    if (entry.dates) {
-                        elem.append($('<span class="dates"></span>').text(entry.dates));
-                    }
-
-                    updateSpans(key, entry.name + ' (' + entry.dates + ')');
-                }
+    query(KEYWORD_API, "id", $(".keywords li").toArray(), function(elem, entry) {
+        if (entry.name) {
+            elem.find("a").text(entry.name['#text']);
+            console.log(entry);
+            if (entry.definition['#text']) {
+                elem.append($('<span class="info"></span>').text(entry.definition['#text']));
             }
-        });
+        }
+
+        updateSpans(key, entry.name['#text']);
     });
 
-    $(".organizations li").each(function() {
-        var elem = $(this);
-        var key = elem.attr("data-ref");
-        $.ajax({
-            url: PERSON_API,
-            data: {
-                "query": key
-            },
-            dataType: "json",
-            success: function(entry) {
-                if (entry.name) {
-                    elem.find("a").text(entry.name);
-                    if (entry.type) {
-                        elem.append($('<span class="dates"></span>').text(entry.type));
-                    }
-
-                    updateSpans(key, entry.name + ' (' + entry.type + ')');
-                }
+    query(LEMMA_API, "id", $(".lemmata li").toArray(), function(elem, entry) {
+        if (entry.morphology) {
+            var label = entry.stdName['#text'] + ' (' +
+                entry.morphology + ')';
+            elem.find("a").text(label);
+            if (entry.definition) {
+                elem.append($('<span class="info"></span>').text(': ' + entry.definition['#text']));
             }
-        });
-    });
-
-    $(".keywords li").each(function() {
-        var elem = $(this);
-        var key = elem.attr("data-ref");
-        $.ajax({
-            url: KEYWORD_API,
-            data: {
-                "id": key
-            },
-            dataType: "json",
-            success: function(entry) {
-                if (entry.type) {
-                    elem.find("a").text(entry.name['#text']);
-                }
-
-                updateSpans(key, entry.name['#text']);
-            }
-        });
-    });
-
-    $(".lemmata li").each(function() {
-        var elem = $(this);
-        var key = elem.attr("data-ref");
-        $.ajax({
-            url: LEMMA_API,
-            data: {
-                "id": key
-            },
-            dataType: "json",
-            success: function(entry) {
-                if (entry.morphology) {
-                    var label = entry.stdName['#text'] + ' (' +
-                        entry.morphology + ')';
-                    elem.find("a").text(label);
-                    if (entry.definition) {
-                        elem.append($('<span class="dates"></span>').text(': ' + entry.definition['#text']));
-                    }
-                    updateSpans(key, label);
-                }
-            }
-        });
+            updateSpans(key, label);
+        }
     });
 });
