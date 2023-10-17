b226b2475c1e62422ce3f5c9d13beeae0ee658fd
Bpolitycki <bastian.politycki@unisg.ch>
2023-10-17 13:35:42 +0200

    build: compile scss using python (libsass)

--- cli/main.py
+++ cli/main.py
@@ -4,6 +4,7 @@ from cli import config
 from cli.volumes import config_reader as vol_config
 from cli.volumes import handle as vol_handle
 from cli.misc_data import handle as misc_handle
+from cli.sass import handle as sass_handle
 from loguru import logger
 import subprocess
 
@@ -24,6 +25,7 @@ def build(
     )
 ):
     logger.info("Starting build process")
+
     if update_data or config.VOLUMES_SOURCE.exists() is False:
         logger.info("Updating / fetching tei-volumes submodule - this may take a while")
         subprocess.run(
@@ -43,6 +45,7 @@ def build(
 
     vol_handle.handle_volumes(vol_config.read_config(), config.VOLUMES_TARGET)
     misc_handle.copy_misc_data(config.MISC_DATA_SOURCE, config.MISC_DATA_TARGET)
+    sass_handle.compile_sass_to_css(config.SASS_SOURCE, config.CSS_TARGET)
 
 
 if __name__ == "__main__":
