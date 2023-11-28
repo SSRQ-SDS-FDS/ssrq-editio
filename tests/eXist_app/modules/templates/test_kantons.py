168f4f011e9182ed950012fd306aaac0df9a765a
Bpolitycki <bastian.politycki@unisg.ch>
2023-11-28 16:36:27 +0100

    test: result of kantons:list function

---
+++ tests/eXist_app/modules/templates/test_kantons.py
@@ -0,0 +1,29 @@
+import pytest
+
+from tests.eXist_app.conftest import (
+    build_query,
+    xquery_modules,
+    xquery_tester,
+)
+
+from cli.config import PROJECT_ROOT, EXIST_APP_DIR
+import json
+from parsel import Selector
+
+from httpx import codes
+
+
+@pytest.mark.asyncio_cooperative
+async def test_kantons_list_returns_one_row_per_kanton(execute_xquery: xquery_tester):
+    xquery = build_query(
+        modules=[xquery_modules["kantons"]],
+        query_body="kantons:list(<div/>, map{})",
+    )
+    response = await execute_xquery(xquery)
+
+    assert response.status_code == codes.OK
+
+    with open(PROJECT_ROOT / EXIST_APP_DIR / "resources/json/cantons.json") as kantons_file:
+        kantons = json.load(kantons_file)
+
+    assert len(Selector(response.text).xpath("//tr")) == len(kantons.keys())
