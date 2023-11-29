46c23b517a68a561fe2b0fce8fcbe93c6505b781
Bpolitycki <bastian.politycki@unisg.ch>
2023-11-29 16:40:44 +0100

    feat: reimplement doc-lists and paratext-view
    This commit reimplements the document-listing as well as the paratext-view with the new routing mechanism based on roaster. Furthermore it improves the code quality by seperating templating from business logic.

--- tests/eXist_app/modules/repository/test_finder.py
+++ tests/eXist_app/modules/repository/test_finder.py
@@ -1,11 +1,23 @@
 import pytest
+from httpx import codes
+from pytest_asyncio_cooperative import Lock
 
 from tests.eXist_app.conftest import (
+    assert_xquery_result,
     build_query,
+    cast_query_result,
+    unquote_xquery_result,
     xquery_modules,
     xquery_tester,
-    assert_xquery_result,
 )
+
+acess_lock = Lock()
+
+
+@pytest.fixture(scope="function")
+async def pdf_access_lock():
+    async with acess_lock():
+        yield
 
 
 @pytest.mark.asyncio_cooperative
@@ -59,7 +71,6 @@ async def test_find_by_idno_against_editio_data(
         ("I_2_8-11.0-1", "SSRQ-FR-I_2_8-11.0-1"),
         ("NE-3-6-1", "SDS-NE-3-6-1"),
         ("NE-4-1.A.1-1", "SDS-NE-4-1.A.1-1"),
-        ("Foo-bar", None),
     ],
 )
 async def test_find_article_by_idno_ending(
@@ -111,3 +122,45 @@ async def test_find_paratext_by_idno_against_editio_data(
     response = await execute_xquery(xquery)
 
     assert_xquery_result(response, expected)
+
+
+@pytest.mark.asyncio_cooperative
+@pytest.mark.parametrize(
+    "idno, expected_filename",
+    [
+        ("SSRQ-SG-III_4-1-1", "SG/III_4/1-1.pdf"),
+        ("SSRQ-FR-I_2_8-11.0-1", "FR/I_2_8/11-1.pdf"),
+        ("SSRQ-FR-I_2_8-11.1-1", "FR/I_2_8/11-1.pdf"),
+        ("SDS-NE-4-1.A.1-1", "NE/4/1.A.1-1.pdf"),
+    ],
+)
+async def test_find_pdf_by_idno_against_editio_data(
+    execute_xquery: xquery_tester, idno: str, expected_filename: str | None, pdf_access_lock: Lock
+):
+    """Test if PDF is available, as expected, and if the filename is correct.
+
+    Note:
+        The access to the PDFs is locked, so that only one test can access the PDFs at a time.
+        So we need the `pdf_access_lock` fixture here.
+    """
+    xquery = build_query(
+        modules=[xquery_modules["config"], xquery_modules["finder"], xquery_modules["ssrq-cache"]],
+        query_body=f"""let $doc:= ssrq-cache:load-from-static-cache-by-id($config:static-cache-path, $config:static-docs-list, "{idno}")
+        let $res := find:pdf-by-idno("{idno}", $doc)
+        return
+            ($res?available, $res?filename)""",  # noqa
+    )
+    response = await execute_xquery(xquery)
+
+    assert response.status_code == codes.OK
+
+    result = [unquote_xquery_result(i) for i in response.text.split("\n") if i != ""]
+
+    if expected_filename is not None:
+        assert cast_query_result(result[0], True)
+        assert (
+            cast_query_result(result[1].replace("/exist/apps/ssrq/", ""), expected_filename)
+            == expected_filename
+        )
+    else:
+        assert not cast_query_result(result[0], False)
