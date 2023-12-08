fc6325e52e0ce67b82497688399f2da5055bc167
Bpolitycki <bastian.politycki@unisg.ch>
2023-12-08 14:10:09 +0100

    feat: added a watch option to the cli compile css command

--- cli/css/handle.py
+++ cli/css/handle.py
@@ -4,11 +4,13 @@ import pytailwindcss
 import subprocess
 
 
-def compile_css(css_source: Path, css_target: Path):
+def compile_css(css_source: Path, css_target: Path, watch: bool = False):
     logger.info(f"Compiling {css_source} to {css_target}")
     try:
         pytailwindcss.run(
-            tailwindcss_cli_args=f"--input {css_source} --output {css_target} --minify"
+            tailwindcss_cli_args=f"--input {css_source} --output {css_target} --minify {'--watch' if watch else ''}",  # noqa
+            cwd=css_source.parent,
+            live_output=True,
         )
     except subprocess.CalledProcessError as e:
         logger.error(f"Failed to compile {css_source} to {css_target} with error: {e}")
