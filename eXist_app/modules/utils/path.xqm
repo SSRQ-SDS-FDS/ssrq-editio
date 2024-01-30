77767acec623f942f98b3b707fd19809daa57841
Bpolitycki <bastian.politycki@unisg.ch>
2024-01-30 17:43:50 +0100

    feat: path utility function to extract a filename (if there is one)

--- eXist_app/modules/utils/path.xqm
+++ eXist_app/modules/utils/path.xqm
@@ -2,6 +2,24 @@ xquery version "3.1";
 
 module namespace path="http://ssrq-sds-fds.ch/exist/apps/ssrq/utils/path";
 
+
+(:~
+: Returns the name of the file denoted by the given path.
+: If the given path denotes a directory, the empty sequence is returned.
+:
+: @param $path the path to extract the file name from as xs:string
+: @return the file name as xs:string?
+:)
+declare function path:get-filename($path as xs:string) as xs:string? {
+    let $components := path:tokenize($path)
+    let $name := $components[last()]
+    return
+        if (path:is-file-name($name)) then
+            path:remove-file-extension($name, path:extract-file-extension($name))
+        else
+            ()
+};
+
 (:~
  : Tells if the given argument denotes a file name, i.e. is not a path.
  :
@@ -25,4 +43,25 @@ declare function path:is-file-name($name as xs:string) as xs:boolean {
  :)
 declare function path:tokenize($path as xs:string) as xs:string* {
     ($path => tokenize("/"))[. != ""]
+};
+
+(:~
+: Extracts the file extension from a path.
+:
+: @param $path as xs:string
+: @return the file extension as xs:string
+:)
+declare function path:extract-file-extension($path as xs:string) as xs:string {
+    tokenize($path, "\.")[last()]
+};
+
+(:~
+: Removes the file extension from a file name.
+:
+: @param $name as xs:string
+: @param $extension as xs:string
+: @return the file name without the extension as xs:string
+:)
+declare function path:remove-file-extension($name as xs:string, $extension as xs:string) as xs:string {
+    replace($name, '\.' || $extension || '$', '')
 };
--- tests/eXist_app/modules/utils/test_path.py
+++ tests/eXist_app/modules/utils/test_path.py
@@ -12,6 +12,25 @@ from tests.eXist_app.conftest import (
 @pytest.mark.parametrize(
     "name, expected",
     [
+        ("foo.xml", "foo"),
+        ("bar/foo", ""),
+        ("baz/bar/foo.html", "foo"),
+    ],
+)
+async def test_get_filename(execute_xquery: xquery_tester, name: str, expected: str):
+    xquery = build_query(
+        modules=[xquery_modules["path"]],
+        query_body=f"""path:get-filename("{name}")""",  # noqa
+    )
+    response = await execute_xquery(xquery)
+
+    assert_xquery_result(response, expected)
+
+
+@pytest.mark.asyncio_cooperative
+@pytest.mark.parametrize(
+    "name, expected",
+    [
         ("/SG", False),
         ("/SG/III_4/", False),
         ("/SG/III_4/intro.html", False),
@@ -47,3 +66,32 @@ async def test_path_tokenize(
     response = await execute_xquery(xquery)
 
     assert_xquery_result(response, expected)
+
+
+@pytest.mark.asyncio_cooperative
+@pytest.mark.parametrize(
+    "name, expected",
+    [
+        ("foo.xml", "xml"),
+        ("bar.html", "html"),
+    ],
+)
+async def test_extract_file_extension(execute_xquery: xquery_tester, name: str, expected: str):
+    xquery = build_query(
+        modules=[xquery_modules["path"]],
+        query_body=f"""path:extract-file-extension("{name}")""",  # noqa
+    )
+    response = await execute_xquery(xquery)
+
+    assert_xquery_result(response, expected)
+
+
+@pytest.mark.asyncio_cooperative
+async def test_remove_file_extension(execute_xquery: xquery_tester):
+    xquery = build_query(
+        modules=[xquery_modules["path"]],
+        query_body=f"""path:remove-file-extension("foo.html", "html")""",  # noqa
+    )
+    response = await execute_xquery(xquery)
+
+    assert_xquery_result(response, "foo")
