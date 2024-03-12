b029e177d11fedbfd847be194ccd1b182258f69f
Bpolitycki <bastian.politycki@unisg.ch>
2024-03-12 16:48:55 +0100

    build: fail build, when css compiling fails

--- cli/css/handle.py
+++ cli/css/handle.py
@@ -2,20 +2,24 @@ from pathlib import Path
 from loguru import logger
 import pytailwindcss
 import subprocess
+import typer
 
 
 def compile_css(css_source: Path, css_target: Path, watch: bool = False):
     logger.info(f"Compiling {css_source} to {css_target}")
     try:
-        pytailwindcss.run(
+        res = pytailwindcss.run(
             tailwindcss_cli_args=f"--input {css_source} --output {css_target} --minify {'--watch' if watch else ''}",  # noqa
             cwd=css_source.parent,
             live_output=True,
             auto_install=True,
             version="latest",
         )
+        if res.returncode != 0:
+            raise subprocess.CalledProcessError(res.returncode, res.args)
     except subprocess.CalledProcessError as e:
         logger.error(f"Failed to compile {css_source} to {css_target} with error: {e}")
+        raise typer.Exit(1)
 
 
 def save_css(filename: Path, content: str):
