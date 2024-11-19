a02b3f2b20694dd7a7efbd05a697248dbe6235ef
Bpolitycki <bastian.politycki@unisg.ch>
2024-11-19 18:38:10 +0100

    test: lang dependency independent from app

---
+++ tests/ssrq_editio/entrypoints/app/shared/test_dependencies.py
@@ -0,0 +1,17 @@
+import pytest
+from ssrq_editio.entrypoints.app.shared.dependencies import get_lang
+from ssrq_utils.lang.display import Lang
+
+
+@pytest.mark.asyncio_cooperative
+@pytest.mark.parametrize(
+    ("x_lang", "lang", "expected_lang"),
+    [
+        ("en", None, Lang.EN),
+        (None, "fr", Lang.FR),
+        (None, None, Lang.DE),
+    ],
+)
+async def test_get_lang(x_lang: str | None, lang: str | None, expected_lang: Lang):
+    result = await get_lang(x_lang=x_lang, lang=lang)
+    assert result == expected_lang
