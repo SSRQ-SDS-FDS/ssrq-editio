854bf1c630a7c425c287e6c4c53fb70390d2d720
Bpolitycki <bastian.politycki@unisg.ch>
2024-11-25 10:38:48 +0100

    chore: renamed queries for storing kanton data

--- src/ssrq_editio/adapters/db/kantons.py
+++ src/ssrq_editio/adapters/db/kantons.py
@@ -6,14 +6,14 @@ from ssrq_editio.adapters.db.config import SQL_DATA_DIR
 from ssrq_editio.adapters.file import load
 
 # The queries need to be executed in the correct order.
-KANTON_DATA_QUERIES = (
-    SQL_DATA_DIR / "kantons.sql",
-    SQL_DATA_DIR / "kanton_images.sql",
+PUT_KANTON_DATA_QUERIES = (
+    SQL_DATA_DIR / "put_kantons.sql",
+    SQL_DATA_DIR / "put_kanton_images.sql",
 )
 
 
 async def initialize_kanton_data(
-    connection: aiosqlite.Connection, table_queries: tuple[Path, ...] = KANTON_DATA_QUERIES
+    connection: aiosqlite.Connection, table_queries: tuple[Path, ...] = PUT_KANTON_DATA_QUERIES
 ):
     async with connection.cursor() as cursor:
         for table_query in table_queries:
