42e291d907a17dd6788341b73296fa9766292ed3
Bpolitycki <bastian.politycki@unisg.ch>
2024-12-02 19:57:36 +0100

    test: index html for cards and title

--- tests/ssrq_editio/conftest.py
+++ tests/ssrq_editio/conftest.py
@@ -1,8 +1,16 @@
 from pathlib import Path
 
 import pytest
+from ssrq_utils.i18n.translator import Translator
+
+from ssrq_editio.entrypoints.app.config import TRANSLATION_SOURCE
 
 
 @pytest.fixture
 def example_path():
     return Path(__file__).parent / "examples"
+
+
+@pytest.fixture(scope="session")
+def translator():
+    return Translator(TRANSLATION_SOURCE)
--- tests/ssrq_editio/entrypoints/app/routers/html/test_main.py
+++ tests/ssrq_editio/entrypoints/app/routers/html/test_main.py
@@ -1,6 +1,8 @@
 import pytest
 from httpx import AsyncClient
 from httpx._status_codes import codes
+from parsel import Selector
+from ssrq_utils.lang.display import Lang
 
 
 @pytest.mark.asyncio_cooperative
@@ -38,3 +40,35 @@ async def test_index_html_has_lang(app_client: AsyncClient, lang: str):
     response = await app_client.get("/", headers={"X-Lang": lang}, params={"lang": "it"})
     assert response.status_code == codes.OK
     assert f'lang="{lang}"' in response.text
+
+
+@pytest.mark.asyncio_cooperative
+@pytest.mark.parametrize(
+    ("lang"),
+    [
+        ("de"),
+        ("en"),
+        ("fr"),
+    ],
+)
+async def test_index_html_has_i18n_title_and_description(
+    app_client: AsyncClient, lang: str, translator
+):
+    response = await app_client.get("/", params={"lang": lang})
+    assert response.status_code == codes.OK
+    doc = Selector(text=response.text)
+    html_title = doc.css("title::text").get()
+    assert html_title is not None
+    assert html_title == translator.translate(Lang.from_string(lang), "short_title")
+    description = doc.css('meta[name="description"]::attr(content)').get()
+    assert description is not None
+    assert description == translator.translate(Lang.from_string(lang), "title")
+
+
+@pytest.mark.asyncio_cooperative
+async def test_index_html_has_kanton_cards(app_client: AsyncClient):
+    response = await app_client.get("/")
+    assert response.status_code == codes.OK
+    doc = Selector(text=response.text)
+    cards = doc.css(".kanton-card").getall()
+    assert len(cards) == 23  # one card for every kanton
