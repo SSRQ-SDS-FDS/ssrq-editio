7e03ff11f3077433906012f33241d8f3eb36d486
Bpolitycki <bastian.politycki@unisg.ch>
2024-12-05 17:55:41 +0100

    feat: api endpoint for serving volume pdfs

--- src/ssrq_editio/entrypoints/app/routers/api/version_one.py
+++ src/ssrq_editio/entrypoints/app/routers/api/version_one.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import StreamingResponse
 
 from ssrq_editio.adapters.db.kantons import list_kantons_abbreviations
 from ssrq_editio.adapters.db.volumes import list_volumes_with_editors
 from ssrq_editio.entrypoints.app.shared.dependencies import DBDependency
+from ssrq_editio.entrypoints.cli.config import VOLUME_SRC
 from ssrq_editio.models.kantons import KantonName
 from ssrq_editio.models.volumes import Volumes
+from ssrq_editio.services.volumes import stream_volume_pdf
 
 version_one = APIRouter(prefix="/v1", tags=["v1"])
 
@@ -28,3 +31,21 @@ async def volumes(connection: DBDependency, kanton: KantonName) -> Volumes:
     if result is None:
         raise HTTPException(status_code=404, detail=f"No data available for »{kanton}«.")
     return Volumes(volumes=tuple(result))
+
+
+@version_one.get(
+    "/kantons/{kanton}}/{volume}.pdf", name="api_v1_volume_pdf", response_class=StreamingResponse
+)
+async def volume_pdf(
+    kanton: KantonName,
+    volume: str,
+    connection: DBDependency,
+) -> StreamingResponse:
+    """Streams the PDF of a specific volume."""
+    try:
+        return StreamingResponse(
+            await stream_volume_pdf(kanton, volume, connection, VOLUME_SRC),
+            media_type="application/pdf",
+        )
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
--- src/ssrq_editio/entrypoints/app/routers/html/main.py
+++ src/ssrq_editio/entrypoints/app/routers/html/main.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 
+from ssrq_editio.entrypoints.app.routers.api import api
 from ssrq_editio.entrypoints.app.shared.dependencies import DBDependency, LangDependency
 from ssrq_editio.entrypoints.app.views.models.index import IndexViewModel
+from ssrq_editio.entrypoints.app.views.models.kanton import KantonViewModel
+from ssrq_editio.models.kantons import KantonName
 
 html = APIRouter(default_response_class=HTMLResponse, include_in_schema=False)
 
@@ -20,8 +23,18 @@ async def search(request: Request, lang: LangDependency, query: str | None):
 
 
 @html.get("/{kanton}", name="kanton")
-async def volumes(kanton: str, request: Request, lang: LangDependency, connection: DBDependency):
-    raise NotImplementedError
+async def volumes(
+    kanton: KantonName, request: Request, lang: LangDependency, connection: DBDependency
+):
+    return await KantonViewModel(request, lang, connection, str(kanton)).to_html()
+
+
+@html.get("/{kanton}/{volume}.pdf", name="volume_pdf")
+async def volume_pdf(
+    kanton: str,
+    volume: str,
+):
+    return RedirectResponse(api.url_path_for("api_v1_volume_pdf", kanton=kanton, volume=volume))
 
 
 @html.get("/{kanton}/{volume}", name="document_list")
