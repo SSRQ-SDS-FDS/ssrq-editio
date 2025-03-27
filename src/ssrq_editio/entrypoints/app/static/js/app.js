e18249bb974542065285b0b5d2598d8a411b5993
Bpolitycki <bastian.politycki@unisg.ch>
2025-03-27 10:48:37 +0100

    fix: htmx param handling and link construction in various places

--- src/ssrq_editio/entrypoints/app/static/js/app.js
+++ src/ssrq_editio/entrypoints/app/static/js/app.js
@@ -1,7 +1,13 @@
 // External imports
 import 'https://cdn.jsdelivr.net/npm/alpinejs@3.13.5/dist/cdn.min.js';
 import 'https://unpkg.com/htmx.org@2.0.0/dist/htmx.min.js';
-// Internal imports
-import topButtonScrollHandler from './components/toTop.js';
 
+// Internal imports
+import langSwitch from './components/langSwitch.js';
+import topButtonScrollHandler from './components/toTop.js';
+import { removeEmptyParameters } from './utils/eventHelpers.js';
+
+// Global setup of event listeners
+Alpine.data('langSwitch', langSwitch);
 Alpine.data('topButtonScrollHandler', topButtonScrollHandler);
+document.addEventListener('htmx:configRequest', removeEmptyParameters);
