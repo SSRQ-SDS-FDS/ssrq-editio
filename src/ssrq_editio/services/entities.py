517556bd8386711446a0b83d6bad01ef75b9033b
Bpolitycki <bastian.politycki@unisg.ch>
2025-04-01 11:15:20 +0200

    fix: change signature of location resolving service and adopt test(s)

--- src/ssrq_editio/services/entities.py
+++ src/ssrq_editio/services/entities.py
@@ -64,8 +64,8 @@ def validate_entity_id(entity_id: str) -> bool:
 
 
 async def resolve_places_for_entities(
-    paged_entities: tuple[Sequence[Entity], list[int] | None], connection: Connection, lang: Lang
-) -> tuple[Sequence[Entity], list[int] | None]:
+    entities: Sequence[Entity], connection: Connection, lang: Lang
+) -> Sequence[Entity]:
     """A service function to resolve and replace place IDs in the 'location' property of each entity with localized place names.
 
     The resolved names are sorted alphabetically in the specified language.
@@ -80,19 +80,20 @@ async def resolve_places_for_entities(
     Returns:
         tuple[tuple[Entity, Sequence[str] | None], ...]: A tuple of entities paired with their resolved and localized place names.
     """
-    if paged_entities:
-        places = cast(Places, await get_entities(connection, EntityTypes.PLACES))
-        if len(places.entities) == 0:
-            raise ValueError("No places found in the database for resolving.")
-        for entity in paged_entities[0]:
-            if isinstance(entity, Family):
-                entity.location = list(
-                    uca_simple_sort(
-                        [
-                            place.get_name_by_lang(lang)
-                            for location in entity.location or []
-                            if (place := places.get_by_id(location))
-                        ]
-                    )
+    places = cast(Places, await get_entities(connection, EntityTypes.PLACES))
+
+    if len(places.entities) == 0:
+        raise ValueError("No places found in the database for resolving.")
+
+    for entity in entities:
+        if isinstance(entity, Family):
+            entity.location = list(
+                uca_simple_sort(
+                    [
+                        place.get_name_by_lang(lang)
+                        for location in entity.location or []
+                        if (place := places.get_by_id(location))
+                    ]
                 )
-    return paged_entities
+            )
+    return entities
--- tests/ssrq_editio/services/test_entities.py
+++ tests/ssrq_editio/services/test_entities.py
@@ -1,39 +1,40 @@
+from typing import cast
 import pytest
 from ssrq_utils.lang.display import Lang
 
 from ssrq_editio.entrypoints.app.shared.dependencies import db_connection
-from ssrq_editio.models.entities import Entities, EntityTypes
+from ssrq_editio.models.entities import Entities, EntityTypes, Family
 from ssrq_editio.services.entities import get_entities, resolve_places_for_entities
 from ssrq_editio.services.paginate import create_pages
 from ssrq_editio.services.sort import sort_entities_by_name
 
 
 @pytest.mark.anyio
-async def test_resolve_places_for_entities():
-    test_values = [
+@pytest.mark.parametrize(
+    "family_id,expected_location_ids,expected_location_names",
+    [("org009447", ["loc000140"], ["Elsass"])],
+)
+async def test_resolve_places_for_entities(
+    db_with_entities,
+    family_id: str,
+    expected_location_ids: list[str],
+    expected_location_names: list[str],
+):
+    """test_values = [
         {"id": "org009447", "location": ["loc000140"], "resolved": ["Elsass"]},
         {
             "id": "org009410",
             "location": ["loc000088", "loc001060"],
             "resolved": ["Freiburg", "Luzern"],
         },
-    ]
-    async for conn in db_connection():
-        result: Entities = await get_entities(
-            conn,
-            EntityTypes.FAMILIES,
-        )
-        assert result is not None
-        for test_value in test_values:
-            assert result.get_by_id(test_value["id"]).location == test_value["location"]
-        paged_entities = create_pages(
-            sort_entities_by_name(entities=result.entities, lang=Lang.DE),
-            1,
-            len(result.entities),
-        )
-        assert paged_entities is not None
-        resolved_entities = await resolve_places_for_entities(paged_entities, conn, Lang.DE)
-        assert resolved_entities is not None
-        for test_value in test_values:
-            assert result.get_by_id(test_value["id"]).location == test_value["resolved"]
-        break
+    ]"""
+    families = await get_entities(
+        db_with_entities,
+        EntityTypes.FAMILIES,
+    )
+    assert families is not None
+    family: Family | None = cast(Family, families.get_by_id(family_id))
+    assert family is not None
+    assert family.location == expected_location_ids  # type: ignore
+    resolved_entities = await resolve_places_for_entities((family,), db_with_entities, Lang.DE)
+    assert resolved_entities[0].location == expected_location_names  # type: ignore
