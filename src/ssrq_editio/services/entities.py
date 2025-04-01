bff67459c88418a2efd88d3b56750c9fbab8b5bb
Alexader Häberlin <alexanderchristian.haeberlin@unisg.ch>
2025-04-01 16:21:38 +0200

    fix: resolved location names are stored in seprate list.

--- src/ssrq_editio/services/entities.py
+++ src/ssrq_editio/services/entities.py
@@ -13,7 +13,15 @@ from ssrq_editio.adapters.db.entities import (
     search_persons,
     search_places,
 )
-from ssrq_editio.models.entities import Entities, Entity, EntityTypes, Family, Places
+from ssrq_editio.models.entities import (
+    Entities,
+    Entity,
+    EntityTypes,
+    Family,
+    Organization,
+    Person,
+    Places,
+)
 
 ENTITY_ID_PATTERN = re.compile(r"^(key|lem|loc|per|org)(\d{6})$")
 
@@ -65,7 +73,7 @@ def validate_entity_id(entity_id: str) -> bool:
 
 async def resolve_places_for_entities(
     entities: Sequence[Entity], connection: Connection, lang: Lang
-) -> Sequence[Entity]:
+) -> tuple[tuple[Entity, list[dict[str, str]] | None], ...]:
     """A service function to resolve and replace place IDs in the 'location' property of each entity with localized place names.
 
     The resolved names are sorted alphabetically in the specified language.
@@ -80,20 +88,29 @@ async def resolve_places_for_entities(
     Returns:
         tuple[tuple[Entity, Sequence[str] | None], ...]: A tuple of entities paired with their resolved and localized place names.
     """
-    places = cast(Places, await get_entities(connection, EntityTypes.PLACES))
-
-    if len(places.entities) == 0:
-        raise ValueError("No places found in the database for resolving.")
-
-    for entity in entities:
-        if isinstance(entity, Family):
-            entity.location = list(
-                uca_simple_sort(
+    allowed_types = (Family, Person, Organization)
+    if all(isinstance(entity, allowed_types) for entity in entities):
+        places = cast(Places, await get_entities(connection, EntityTypes.PLACES))
+        if len(places.entities) == 0:
+            raise ValueError("No places found in the database for resolving.")
+        resolved_entities = []
+        typed_entities = cast(Sequence[Family], entities)
+        for entity in typed_entities:
+            resolved_location = {
+                place.get_name_by_lang(lang): location
+                for location in entity.location or []
+                if (place := places.get_by_id(location))
+            }
+            sorted_location = uca_simple_sort(list(resolved_location.keys()))
+            resolved_entities.append(
+                (
+                    entity,
                     [
-                        place.get_name_by_lang(lang)
-                        for location in entity.location or []
-                        if (place := places.get_by_id(location))
-                    ]
+                        {"id": resolved_location[location], "name": location}
+                        for location in sorted_location
+                    ],
                 )
             )
-    return entities
+        return tuple(resolved_entities)
+    else:
+        return tuple((entity, None) for entity in entities)
