34abaea34d600af71a2cae44664403a0ff22ec87
Bpolitycki <bastian.politycki@unisg.ch>
2025-04-17 17:38:34 +0200

    fix: query to list volumes per kanton
    The previous implementation only used to list 1 volume per kanton. This
    behaviour was not catched by the test, which is now also adopted.

--- src/ssrq_editio/adapters/db/sql/data/get_volumes.sql
+++ src/ssrq_editio/adapters/db/sql/data/get_volumes.sql
@@ -9,13 +9,15 @@ SELECT
     GROUP_CONCAT(e.name, ',') AS editors
 FROM
     volumes AS v
-LEFT JOIN
-    editors AS e
-    ON v.id = e.volume_id
 INNER JOIN
     kantons AS k
     ON v.kanton_id = k.id
+LEFT JOIN
+    editors AS e
+    ON v.id = e.volume_id
 WHERE
     k.short_name = ?
+GROUP BY
+    v.id, v.name, k.short_name, v.title, v.prefix, v.pdf, v.literature
 ORDER BY
     v.id ASC;
