d928c20a18173e5286f8078c59db1aeb03f3bf45
Alexader Häberlin <alexanderchristian.haeberlin@unisg.ch>
2025-06-13 16:46:25 +0200

    chore: migrate CSS from XSLT to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  safelist: ['list-decimal'],
   content: ['./src/ssrq_editio/entrypoints/app/**/*.{html,jinja,xsl,json}'],
   darkMode: 'media',
   theme: {
