4f8b442d387022fa44f8bfb91b5017acfb9cbde1
Bpolitycki <bastian.politycki@unisg.ch>
2025-08-13 14:29:52 +0200

    feat: allow additional data snippets for nested templates

--- src/ssrq_editio/entrypoints/app/views/utils.py
+++ src/ssrq_editio/entrypoints/app/views/utils.py
@@ -1,14 +1,21 @@
-from typing import cast
+from typing import Any, cast
 
+from fastapi import Request
 from jinja2 import pass_context
 from jinja2.runtime import Context
+from jinjax import Catalog
 from markupsafe import Markup
+from ssrq_utils.i18n.translator import Translator
+from ssrq_utils.lang.display import Lang
 
-__all__ = ["render_template_string"]
+from ssrq_editio.models.entities import Entities, EntityTypes
+from ssrq_editio.services.entities import map_to_entity_type
+
+__all__ = ["create_entity_preview_by_id", "render_template_string"]
 
 
 @pass_context
-def render_template_string(context: Context, value: str) -> Markup:
+def render_template_string(context: Context, value: str, data: dict[str, Any] | None) -> Markup:
     """
     Render a Jinja2 template string with the given context.
 
@@ -20,7 +27,49 @@ def render_template_string(context: Context, value: str) -> Markup:
         Markup: The rendered template as a Markup object.
     """
     _template = context.eval_ctx.environment.from_string(value)
-    result = _template.render(**context)
+    result = _template.render(**context if data is None else {**context, **data})
     if context.eval_ctx.autoescape:
         result = Markup(result)
     return cast(Markup, result)
+
+
+def create_entity_preview_by_id(
+    component_catalog: Catalog,
+    id: str,
+    entities: dict[EntityTypes, Entities],
+    lang: Lang,
+    translator: Translator,
+    request: Request,
+) -> str:
+    """
+    This function is a utility, which will help us to create entity tooltips
+    from the XSLT output processed by the templating engine of the web app. We will
+    create a template string, which call's this function and is processed by jinja afterwards.
+    Only the web app knows all the information about the entities, which we want to display
+    in the frontend.
+
+    Args:
+        component_catalog (Catalog): The Jinja2 catalog to render the component. Passed by the context.
+        id (str): The ID of the entity. Passed by the XSLT.
+        entities (dict[EntityTypes, Entities]): The entities to search in. Passed by document view as property additional-data.
+        lang (Lang): The language to use for rendering. Passed by document view as property additional-data.
+        translator (Translator): The translator instance. Passed by document view as property additional-data.
+        request (Request): The FastAPI request object. Passed by document view as property additional-data.
+
+    Returns:
+        str: The rendered HTML string for the entity preview / tooltip.
+    """
+
+    entity, entity_type = next(
+        (e, et) for et in map_to_entity_type(id) if (e := entities[et].get_by_id(id)) is not None
+    )
+
+    return component_catalog.render(
+        "EntityPreview",
+        entity=entity,
+        entity_type=entity_type.value,
+        name=entity.get_name_by_lang(lang),
+        lang=lang,
+        translator=translator,
+        request=request,
+    )
