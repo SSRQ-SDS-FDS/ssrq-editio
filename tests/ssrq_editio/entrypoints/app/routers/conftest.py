05af91d2b0b5c656ef58e457a7b8545aef365ed1
Bpolitycki <bastian.politycki@unisg.ch>
2025-10-30 13:39:43 +0100

    test: occurrences API endpoint
    May require more testing for future implementations

--- tests/ssrq_editio/entrypoints/app/routers/conftest.py
+++ tests/ssrq_editio/entrypoints/app/routers/conftest.py
@@ -5,12 +5,17 @@ from aiosqlite import Connection
 from httpx import ASGITransport, AsyncClient
 
 from ssrq_editio.adapters.db.connection import db_session
+from ssrq_editio.adapters.db.documents import initialize_document_data
 from ssrq_editio.adapters.db.entities import store_entities
 from ssrq_editio.adapters.db.kantons import initialize_kanton_data
 from ssrq_editio.adapters.db.setup import setup_db
 from ssrq_editio.adapters.db.volumes import initialize_volume_with_editors
 from ssrq_editio.entrypoints.app.main import app
 from ssrq_editio.entrypoints.app.shared.dependencies import db_connection
+from ssrq_editio.models.documents import (
+    Document,
+    DocumentType,
+)
 from ssrq_editio.models.volumes import Volume
 
 TEST_VOLUMES = [
@@ -27,6 +32,35 @@ TEST_VOLUMES = [
     )
 ]
 
+TEST_DOCUMENTS = (
+    Document(
+        uuid="d56f1ce8-cec9-49ed-b54b-09f397adc2d8",
+        idno="SSRQ-SG-III_4-1-1",
+        is_main=True,
+        sort_key=1,
+        de_orig_date='<span class="tei-origDate">1473 April 26 a. S.</span>',
+        en_orig_date='<span class="tei-origDate">1473 April 26 O.S.</span>',
+        fr_orig_date='<span class="tei-origDate">1473 avril 26 a. s.</span>',
+        it_orig_date='<span class="tei-origDate">1473 aprile 26 v. s.</span>',
+        facs=["OGA_Gams_Nr_5_r", "OGA_Gams_Nr_5_v"],
+        printed_idno="SSRQ SG III/4 1/1",
+        volume_id="SG_III_4",
+        orig_place=["loc000001"],
+        de_title="foo",
+        fr_title=None,
+        entities=[
+            "per031589",
+            "loc000001",
+            "key000001",
+            "lem000001",
+            "org000001",
+        ],
+        type=DocumentType.transcript,
+        start_year_of_creation=1473,
+        end_year_of_creation=None,
+    ),
+)
+
 
 @pytest.fixture(scope="module")
 async def app_db_connection() -> AsyncGenerator[Connection, None]:
@@ -39,6 +73,7 @@ async def app_db_setup(app_db_connection, entities) -> AsyncGenerator[Connection
     await setup_db(app_db_connection)
     await initialize_kanton_data(app_db_connection)
     await initialize_volume_with_editors(app_db_connection, TEST_VOLUMES[0])
+    await initialize_document_data(TEST_DOCUMENTS, app_db_connection)
     await store_entities(entities, app_db_connection)
     yield app_db_connection
 
