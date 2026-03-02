d3adcc9466745b88fb04ac0fde162601f3384046
Bpolitycki <bastian.politycki@unisg.ch>
2026-03-02 15:38:57 +0100

    fix: retrieval of volume date-range
    This commit uses the SQL-COALESCE mechanism to prevent errors happening,
    because some documents are stored without an end-date in the DB.

--- src/ssrq_editio/adapters/db/sql/data/get_volume_meta.sql
+++ src/ssrq_editio/adapters/db/sql/data/get_volume_meta.sql
@@ -1,6 +1,6 @@
 SELECT
-    MIN(docs.start_year_of_creation) AS first_year,
-    MAX(docs.end_year_of_creation) AS last_year,
+    MIN(COALESCE(docs.start_year_of_creation, docs.end_year_of_creation)) AS first_year,
+    MAX(COALESCE(docs.end_year_of_creation, docs.start_year_of_creation)) AS last_year,
     MAX(CASE WHEN docs.facs IS NOT NULL THEN 1 ELSE 0 END) AS has_facs,
     JSON_GROUP_ARRAY(DISTINCT docs.type) AS document_types
 FROM documents AS docs
